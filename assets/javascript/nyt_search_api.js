aff0bd2f76afb629a9f8f1180f7987b1363aad25
Carlos Quinto <carlquin77@gmail.com>
2019-04-26 14:11:57 -0700

    working ajax search

--- assets/javascript/nyt_search_api.js
+++ assets/javascript/nyt_search_api.js
@@ -1,20 +1,24 @@
 $(document).ready(function () {
 
-    var queryURl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q" +
-    parameterToSearch + "&page=2&sort=oldest&api-key=b9b6dd9-652a-46bd-b589-b9ac1d373c83";
+    var queryURl;
 
-    $("#search-submit").on("click", function(event) {
-        // Preventing the button from trying to submit the form
+    $("#search-submit").on("click", function (event) {
+
+        searchTerm = $("#search-term").text();
+
+        console.log(searchTerm);
+        queryURl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+        searchTerm + "&api-key=qXG8wP8eqVtloArktou8tJ6QD1c7HrMz";
+
         event.preventDefault();
         $.ajax({
             url: queryURl,
             method: "GET"
-        }).then(function(response){
+        }).then(function (response) {
             console.log(response);
+            $("#results-panel").append(response.docs);
         });
 
-        
-        
-    console.log("dodododo")
-      });
+        console.log("dodododo")
+    });
 })
