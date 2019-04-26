bb614ff22c816e51da4807409ce71b0d75af7076
Carlos Quinto <carlquin77@gmail.com>
2019-04-26 13:33:16 -0700

    updated html, some paneling and output, updated javascript ajax search functionality

--- assets/javascript/nyt_search_api.js
+++ assets/javascript/nyt_search_api.js
@@ -1,6 +1,20 @@
-
-
 $(document).ready(function () {
 
-    
+    var queryURl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q" +
+    parameterToSearch + "&page=2&sort=oldest&api-key=b9b6dd9-652a-46bd-b589-b9ac1d373c83";
+
+    $("#search-submit").on("click", function(event) {
+        // Preventing the button from trying to submit the form
+        event.preventDefault();
+        $.ajax({
+            url: queryURl,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+        });
+
+        
+        
+    console.log("dodododo")
+      });
 })
